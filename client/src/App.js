e23e1b468be578ab4c6f1c7965cadf77cd8e9ff0
김서짱 <seojeong1101@gmail.com>
2021-11-23 15:13:52 +0900

    frontend directory setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes} from 'react-router-dom';
+
+import About from './pages/About';
+import Question from './pages/Question';
+import Create from './pages/Create';
+import Home from './pages/Home';
+import Orders from './pages/Orders';
+import Checkout from './pages/Checkout';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route exact={true} path ='/' element={<Home/>} />
+        <Route exact={true} path ='/about' element={<About/>} />
+        <Route exact={true} path ='/create' element={<Create/>} />
+        <Route exact={true} path = '/question' element={<Question/>} />
+        <Route exact={true} path='/orders' element={<Orders/>}  />
+        <Route exact={true} path ='/checkout' element={<Checkout/>} />
+      </Routes>
+
     </div>
+    
   );
 }
-
 export default App;
---
+++ client/src/pages/Checkout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Checkout =({history})=>{
+    return(
+        <div>
+            <h1> 체크아웃 페이지</h1>
+        </div>
+    );
+}
+export default Checkout;
