d1d8416c51b6b442ba311d4a5af817436dd9b532
JSKIM <myjunee3608@gmail.com>
2021-11-27 00:12:12 +0900

    connect all pages

--- client/src/components/StepsLetter.js
+++ client/src/components/StepsLetter.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Box, Center, Breadcrumb, BreadcrumbItem, BreadcrumbLink, CloseButton } from '@chakra-ui/react';
 import { ChevronRightIcon } from '@chakra-ui/icons';
+import { useNavigate } from 'react-router';
 
 function StepsLetter() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <Box p='6' w='100%' h='70'>
@@ -11,15 +14,30 @@ function StepsLetter() {
       <Center bg='gray' w='100%' h='100'>
         <Breadcrumb fontSize='2xl' spacing='17' separator={<ChevronRightIcon color='white' />}>
           <BreadcrumbItem>
-            <BreadcrumbLink href='#'>편지쓰기</BreadcrumbLink>
+            <BreadcrumbLink
+              onClick={() => {
+                navigate('/create');
+              }}>
+              편지쓰기
+            </BreadcrumbLink>
           </BreadcrumbItem>
 
           <BreadcrumbItem>
-            <BreadcrumbLink href='#'>시집편집</BreadcrumbLink>
+            <BreadcrumbLink
+              onClick={() => {
+                navigate('/create/edit');
+              }}>
+              시집편집
+            </BreadcrumbLink>
           </BreadcrumbItem>
 
           <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink href='#'>결제하기</BreadcrumbLink>
+            <BreadcrumbLink
+              onClick={() => {
+                navigate('/checkout');
+              }}>
+              결제하기
+            </BreadcrumbLink>
           </BreadcrumbItem>
         </Breadcrumb>
       </Center>
