930790eda2a1220f30114280cafb5bfa518a53f3
[2기-1반] 정율리 <yuliekorea@gmail.com>
2021-12-01 11:41:52 +0000

    [BE] FastAPI 구축 & 라우팅, 모델링.

---
+++ server/project/app/__init__.py
@@ -0,0 +1,3 @@
+# from .db import engine as db_engine
+# from .api import main_router
+# from . import models, schemas
---
+++ server/project/app/config.py
@@ -0,0 +1,18 @@
+"""
+FastAPI server configuration
+"""
+
+from decouple import config
+from pydantic import BaseModel
+
+
+class Settings(BaseModel):
+    """Server config settings"""
+
+    # Database settings
+    mysql_uri = config("SQLALCHEMY_DATABASE_URL")
+    mongo_uri = config("MONGO_URI")
+    mongo_db = config("MONGO_DB")
+
+
+CONFIG = Settings()
