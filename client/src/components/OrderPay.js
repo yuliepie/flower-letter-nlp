d3eb8e5c49186d1d19ae9b9ef955f2e77f035c0c
김서짱 <seojeong1101@gmail.com>
2021-12-02 17:59:21 +0900

    ADD:Page Layout

--- client/src/components/OrderPay.js
+++ client/src/components/OrderPay.js
@@ -6,38 +6,56 @@ import {
   Input,
   Center,
   Button,
+  Text
 } from "@chakra-ui/react";
+import {  useNavigate } from "react-router";
 
-function OrderPay() {
+
+
+
+function OrderPay({history}) {
+  
+  const navigate = useNavigate()
+  
+  const orderInfo = '김서정님 주문 완료 되었습니다.'
+
+  const onClickPayButton = async()=>{
+    navigate('/checkout')
+  }
+
+
   return (
-    <div>
+    <div margin='25px'>
       <Flex
         p="2"
         h="70"
         justifyContent="center"
         alignItems="center"
         borderBottom="1px"
+        marginTop='50px'
+        marginLeft='100px'
+        
       >
         <Box p="4" w="90%">
-          결제하기
+          <Text fontSize='4xl' fontWeight='bold'>결제하기</Text>
         </Box>
         <Box p="1" w="10%">
           <CloseButton size="3xl" />
         </Box>
       </Flex>
 
-      <Flex>
-        <Box p="4" w="67%" h="730">
-          <Box w="100%" h="10%" textAlign="center">
-            주문자정보
+      <Flex marginLeft='100px' marginRight='100px'>
+        <Box p="4" w="67%" h="full">
+          <Box w="100%" h="10%" textAlign="left">
+            <Text fontSize='2xl' fontWeight='bold'>1. 주문자 정보</Text>
           </Box>
-          <Box p="2" w="100%" h="25%" border="1px" borderColor="black">
+          <Box p="2" w="100%" h="25%" border="1px" borderColor="black" >
             <Input w="80%" mb="3" borderColor="black" placeholder="이름" />
             <Input w="80%" mb="3" borderColor="black" placeholder="전화번호" />
             <Input w="80%" mb="3" borderColor="black" placeholder="이메일" />
           </Box>
-          <Box w="100%" h="10%" textAlign="center">
-            배송정보
+          <Box w="100%" h="10%" textAlign="left" marginTop='15px'>
+            <Text fontSize='2xl' fontWeight='bold'>2. 배송 정보</Text>
           </Box>
           <Box p="2" w="100%" h="55%" border="1px" borderColor="black">
             <Input w="80%" mb="3" borderColor="black" placeholder="이름" />
@@ -46,6 +64,7 @@ function OrderPay() {
             <Input w="80%" mb="3" borderColor="black" placeholder="이름" />
             <Input w="80%" mb="3" borderColor="black" placeholder="전화번호" />
           </Box>
+
         </Box>
         <Box p="4" w="33%" h="730">
           <Center p="2" w="100%" h="10%">
@@ -90,7 +109,7 @@ function OrderPay() {
             justifyContent="center"
             alignItems="center"
           >
-            <Button colorScheme="blue" m="1" w="90%">
+            <Button colorScheme="blue" m="1" w="90%" onClick={onClickPayButton}>
               결제하기
             </Button>
           </Flex>
--- client/src/components/WithBackgroundImage.js
+++ client/src/components/WithBackgroundImage.js
@@ -6,36 +6,56 @@ import {
   VStack,
   useBreakpointValue,
   Box,
+  Link
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router";
+
 
 export default function WithBackgroundImage({
   buttonName,
   backgroundImg,
   text,
+  w ='full',
+  h='full',
+  textcolor,
+  backgroundcolor,
+  isbutton=false,
+  buttonColor,
+  link,
+  buttonUrl
 }) {
-  const navigate = useNavigate();
+
+  const navigate = useNavigate()
+
+  const button_Url = buttonUrl 
+
   return (
     <VStack
-      w={"full"}
-      h={"full"}
+      w={w}
+      h={h}
       justify={"center"}
       px={useBreakpointValue({ base: 4, md: 8 })}
       backgroundImage={backgroundImg}
       backgroundPosition="center"
       backgroundSize="cover"
+      backgroundColor={backgroundcolor}
+      
     >
-      <Stack align={"flex-start"} spacing={6} alignItems={"center"}>
+      <Stack align={"flex-start"} spacing={6} alignItems={"center"} whiteSpace={"pre-line"}>
         <Text
-          color={"white"}
+          color={textcolor}
           fontWeight={700}
           lineHeight={1.2}
-          fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
+          fontSize={useBreakpointValue({ base: "3xl", md: "5xl" })}
+          
         >
           {text}
         </Text>
+        
         {/* <Stack direction={"row"}> */}
-        <Button
+        {
+          /*
+          <Button
           bg={"blue.400"}
           rounded={"full"}
           color={"white"}
@@ -43,7 +63,20 @@ export default function WithBackgroundImage({
         >
           {buttonName}
         </Button>
+          */ 
+        }
+        {isbutton ? <Button
+          bg={buttonColor}
+          rounded={"full"}
+          color={"black"}
+          _hover={{ bg: "blue.500" }}
+          onClick={()=>{ navigate(button_Url)}}
+        >
+          {buttonName}
+        </Button>: null}
+        
         {/* </Stack> */}
+        <Link>{link}</Link>
       </Stack>
     </VStack>
   );
