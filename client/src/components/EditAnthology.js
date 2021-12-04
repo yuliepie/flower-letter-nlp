335375dc9a8edc5675c911a7c6cfc836ba2a9c94
JSKIM <myjunee3608@gmail.com>
2021-12-04 20:14:52 +0900

    STYLE: create page button layout

--- client/src/components/EditAnthology.js
+++ client/src/components/EditAnthology.js
@@ -20,14 +20,14 @@ function EditAnthology() {
       <StepsLetter></StepsLetter>
       <HStack h='70vh' p='2' ml='5' mr='5' align='center' justify='center'>
         {/* 왼쪽, 오른쪽 박스를 묶는 박스 */}
-        <Flex w='50%' h='100%' border='1px' borderRadius='10px' mr='1'>
+        <Flex w='60%' h='100%' border='1px' borderRadius='10px' mr='1' p='2'>
           {/* 왼쪽 박스 */}
-          키워드선택단계
+          키워드 선택 단계
         </Flex>
-        <Flex justify='center' w='50%' h='100%' border='1px' borderRadius='10px' ml='1' p='5'>
-          {/* 오른쪽 박스 */}
-          <Box m='2' bg='red'>
-            <Slider aria-label='slider-ex-3' defaultValue={30} orientation='vertical' minH='32'>
+        <Flex justify='center' w='40%' h='100%' border='1px' borderRadius='10px' ml='1' p='5'>
+          {/* 오른쪽 박스 - 키워드 비율 조절 */}
+          <Box m='2' p='3' w='100%' h='40%' align='center'>
+            <Slider aria-label='slider-ex-3' defaultValue={35} orientation='vertical' minH='32' mr='4' ml='4'>
               <SliderTrack>
                 <SliderFilledTrack />
               </SliderTrack>
