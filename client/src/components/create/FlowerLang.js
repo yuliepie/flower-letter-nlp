3efd6dac2eb27e646b5f2746c5550154f1ab681a
JSKIM <myjunee3608@gmail.com>
2021-12-07 00:35:31 +0900

    ADD: layout flowerpage

--- client/src/components/create/FlowerLang.js
+++ client/src/components/create/FlowerLang.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import StepsLetter from '../StepsLetter';
 import EditContainer from './EditContainer';
-import { Flex, HStack,Box,Button,Spacer } from '@chakra-ui/react';
+import { Flex, HStack, Box, Button, Spacer } from '@chakra-ui/react';
 import { useNavigate } from 'react-router';
 
 export default function FlowerLang() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div>
       <StepsLetter />
@@ -15,23 +15,57 @@ export default function FlowerLang() {
           {/* 왼쪽 박스 */}
           꽃말선택 단계
         </Flex>
-        <Flex w='40%' h='100%' border='1px' borderRadius='10px' ml='1'>
+        <Flex w='40%' h='100%' border='1px' borderRadius='10px' ml='1' align='center' justify='center'>
           {/* 오른쪽 박스 */}
+          <div className='flowerbox' align='center'>
+            {/* 스크롤 박스 */}
+            <Button className='flowerbutton' m='2' w='90%' h='100px' bg='skyblue' fontWeight='600' color='white'>
+              Color
+            </Button>
+            <Button className='flowerbutton' m='2' w='90%' h='100px' bg='skyblue' fontWeight='600' color='white'>
+              Color
+            </Button>
+            <Button className='flowerbutton' m='2' w='90%' h='100px' bg='skyblue' fontWeight='600' color='white'>
+              Color
+            </Button>
+            <Button className='flowerbutton' m='2' w='90%' h='100px' bg='skyblue' fontWeight='600' color='white'>
+              Color
+            </Button>
+            <Button className='flowerbutton' m='2' w='90%' h='100px' bg='skyblue' fontWeight='600' color='white'>
+              Color
+            </Button>
+          </div>
         </Flex>
       </HStack>
       <Flex pl='6' pr='6' h='10%' w='100%'>
-      <Box>
-        <Button w='30vh' h='7vh' bg='skyblue' color='white' fontSize='3vh' onClick={() => { navigate('/create/keyword'); }}>
-          이전 단계
-        </Button>
-      </Box>
-      <Spacer />
-      <Box>
-        <Button w='30vh' h='7vh' bg='skyblue' color='white' fontSize='3vh' onClick={() => { navigate('/create/bookcover'); }}> 
-        다음 단계
-        </Button>
-      </Box>
-    </Flex>
+        <Box>
+          <Button
+            w='30vh'
+            h='7vh'
+            bg='skyblue'
+            color='white'
+            fontSize='3vh'
+            onClick={() => {
+              navigate('/create/keyword');
+            }}>
+            이전 단계
+          </Button>
+        </Box>
+        <Spacer />
+        <Box>
+          <Button
+            w='30vh'
+            h='7vh'
+            bg='skyblue'
+            color='white'
+            fontSize='3vh'
+            onClick={() => {
+              navigate('/create/bookcover');
+            }}>
+            다음 단계
+          </Button>
+        </Box>
+      </Flex>
     </div>
   );
 }
