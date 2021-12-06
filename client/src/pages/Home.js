3eef5b722587fbba7fbcd566fcea9b8b2416654c
yuliepie <yuliekorea@gmail.com>
2021-12-06 11:15:18 +0900

    ADD: ping test

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import SidebarComponent from '../components/SidebarComponent';
-import Fullpage from '../components/Fullpage';
+import React from "react";
+import SidebarComponent from "../components/SidebarComponent";
+import Fullpage from "../components/Fullpage";
+
+const axios = require("axios");
 
 const Home = ({ history }) => {
+  async function testPing() {
+    try {
+      const response = await axios.get("https://api.flowerletter.co.kr/ping");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
+      <button onClick={testPing}>Test Ping</button>
       <SidebarComponent></SidebarComponent>
       <Fullpage></Fullpage>
     </>
