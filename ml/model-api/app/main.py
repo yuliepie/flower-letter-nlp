e6df66a13514d46781f5cc3c1a7ba58e99adde16
yuliepie <yuliekorea@gmail.com>
2021-12-06 21:22:52 +0900

    ADD: model api structure

---
+++ ml/model-api/app/main.py
@@ -0,0 +1,32 @@
+from typing import List
+from fastapi import FastAPI
+import pickle
+from pydantic import BaseModel
+
+
+class Letter(BaseModel):
+    text: str
+
+
+class Keywords(BaseModel):
+    keywords: List[str]
+
+
+# import model
+model = pickle.load(open("model.pkl", "rb"))
+
+app = FastAPI(title="API service for model")
+
+
+@app.get("/")
+def read_root():
+    return {"message": "모델 API 입니다!"}
+
+
+@app.get("/classify", response_model=Keywords)
+def classify_letter(data: Letter):
+    # 모델로 키워드 추출 후 반환
+
+    print(data.text)
+
+    return ["우정, 사랑, 여행"]
