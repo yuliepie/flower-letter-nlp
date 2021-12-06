cec5442e284421bed7a47b6ee7180764dd6f4ab0
[2기-1반] 정율리 <yuliekorea@gmail.com>
2021-12-06 12:50:50 +0000

    [BE] 모델 API 뼈대 구축

--- ml/model-api/app/main.py
+++ ml/model-api/app/main.py
@@ -1,4 +1,19 @@
+from typing import List
 from fastapi import FastAPI
+import pickle
+from pydantic import BaseModel
+
+
+class Letter(BaseModel):
+    text: str
+
+
+class Keywords(BaseModel):
+    keywords: List[str]
+
+
+# import model
+model = pickle.load(open("model.pkl", "rb"))
 
 app = FastAPI(title="API service for model")
 
@@ -8,6 +23,10 @@ def read_root():
     return {"message": "모델 API 입니다!"}
 
 
-@app.get("/keywords")
-def read_root():
-    return {"ping": "test!"}
+@app.get("/classify", response_model=Keywords)
+def classify_letter(data: Letter):
+    # 모델로 키워드 추출 후 반환
+
+    print(data.text)
+
+    return ["우정, 사랑, 여행"]
