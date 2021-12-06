df29b9fbc1fa7283199f26e189de9ab31a12fb51
yuliepie <yuliekorea@gmail.com>
2021-12-06 15:48:12 +0900

    REFACTOR: Remove db image from production

--- server/project/prestart.sh
+++ server/project/prestart.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
+# echo "Waiting for postgres..."
 
-while ! nc -z postgres_db 5432; do
-  sleep 0.1
-done
+# while ! nc -z postgres_db 5432; do
+#   sleep 0.1
+# done
 
-echo "PostgreSQL started"
+# echo "PostgreSQL started"
 
-while ! nc -z mongo_db 27017; do
-  sleep 0.1
-done
+# while ! nc -z mongo_db 27017; do
+#   sleep 0.1
+# done
 
-echo "MongoDB started"
+# echo "MongoDB started"
 
-# Migration
-alembic upgrade head
+# # Migration
+# alembic upgrade head
 
 exec "$@"
