5694785b4429f98ae174865d3ea92c1817f9e235
JSKIM <myjunee3608@gmail.com>
2021-12-07 21:31:44 +0900

    STYLE: sidebar styling

--- client/src/components/SidebarComponent.jsx
+++ client/src/components/SidebarComponent.jsx
@@ -1,4 +1,4 @@
-import { Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton,Link,VStack } from '@chakra-ui/react';
+import { Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, Link, VStack, Box, Image } from '@chakra-ui/react';
 import { useDisclosure } from '@chakra-ui/react';
 import React from 'react';
 import { Button } from '@chakra-ui/react';
@@ -14,46 +14,81 @@ function SidebarComponent() {
   return (
     <>
       <Button ref={btnRef} colorScheme='teal' onClick={onOpen} backgroundColor={'#D3B1C2'}>
-        <HamburgerIcon/>
+        <HamburgerIcon />
       </Button>
       <Drawer isOpen={isOpen} placement='left' onClose={onClose} finalFocusRef={btnRef}>
         <DrawerOverlay />
-        <DrawerContent backgroundColor='#FBEDE0'>
-          <DrawerCloseButton />
-          <DrawerHeader fontSize='50px'>꽃편지</DrawerHeader>
-
-          <DrawerBody flexDirection='column' justifyContent='space-between' >
+        <DrawerContent bgGradient='linear(to-b, rgba(212, 187, 221, 1),  rgba(251, 237, 224, 1),  )'>
+          <DrawerCloseButton size='lg' color='white' />
+          <DrawerBody flexDirection='column' justifyContent='space-between'>
             <br />
             <VStack>
-            <Link fontSize='40px'
-              onClick={() => {
-                navigate('/about');
-              }}>
-              About us
-            </Link>
-            <br />
-            <Link fontSize='40px'
-              onClick={() => {
-                navigate('/howtouse');
-              }}>
-              How To use
-            </Link>
-            <br />
-            <Link fontSize='40px'
-              onClick={() => {
-                navigate('/question');
-              }}>
-              FAQ
-            </Link>
-            <br />
-            <Link fontSize='40px'
-              onClick={() => {
-                navigate('/question');
-              }}>
-              문의하기
-            </Link>
-            <br/>
-            <Button onClick={()=>navigate('/start')} backgroundColor="#D3B1C2">시집 만들러</Button>
+              <Link
+                w='160px'
+                h='110px'
+                mb='3'
+                onClick={() => {
+                  navigate('/');
+                }}>
+                <Image src='img/logoimg.png' alt='logo' borderRadius='15px' border='2px' borderColor='#A49393' />
+              </Link>
+              <Button
+                _hover={{ backgroundColor: '#A49393', color: 'white', fontWeight: '600' }}
+                onClick={() => navigate('/start')}
+                w='140px'
+                backgroundColor='white'
+                border='1px'
+                borderColor='#A49393'
+                color='#A49393'
+                borderRadius='20px'
+                fontWeight='500'>
+                시집 만들러 가기
+              </Button>
+              <Box h='10'></Box>
+              <Link
+                _hover={{ color: '#613659', fontWeight: '600' }}
+                fontSize='30px'
+                fontWeight='400'
+                color='#A49393'
+                onClick={() => {
+                  navigate('/about');
+                }}>
+                About us
+              </Link>
+              <br />
+              <Link
+                _hover={{ color: '#613659', fontWeight: '600' }}
+                fontSize='30px'
+                fontWeight='400'
+                color='#A49393'
+                onClick={() => {
+                  navigate('/howtouse');
+                }}>
+                How To use
+              </Link>
+              <br />
+              <Link
+                _hover={{ color: '#613659', fontWeight: '600' }}
+                fontSize='30px'
+                fontWeight='400'
+                color='#A49393'
+                onClick={() => {
+                  navigate('/question');
+                }}>
+                FAQ
+              </Link>
+              <br />
+              <Link
+                _hover={{ color: '#613659', fontWeight: '600' }}
+                fontSize='30px'
+                fontWeight='400'
+                color='#A49393'
+                onClick={() => {
+                  navigate('/question');
+                }}>
+                문의하기
+              </Link>
+              <br />
             </VStack>
           </DrawerBody>
         </DrawerContent>
