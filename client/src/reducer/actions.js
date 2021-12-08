bd9767171d268932247d4bff139553cf00d17b43
김서짱 <seojeong1101@gmail.com>
2021-12-08 13:38:25 +0900

    prettier

--- client/src/reducer/actions.js
+++ client/src/reducer/actions.js
@@ -1,5 +1,5 @@
 // 편지내용 저장
-export const SAVE_LETTER = "SAVE_LETTER";
+export const SAVE_LETTER = 'SAVE_LETTER';
 // 자유글 저장
 export const SAVE_FREECONTENT = 'SAVE_FREECONTENT';
 // 폰트 저장
@@ -12,6 +12,3 @@ export const SAVE_FLOWER_DATA = 'SAVE_FLOWER_DATA';
 export const SAVE_POEMS = 'SAVE_POEMS';
 // 사용자가 선택한 꽃말 저장
 export const SAVE_USER_FLOWER = 'SAVE_USER_FLOWER';
-
-
-
