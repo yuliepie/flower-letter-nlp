0c7eb60508e82a98a8e2f36a32d365abe17a8a8f
[2기 2반] 김서정 <kseoj0921@naver.com>
2021-12-08 04:39:23 +0000

    꽃말 선택 기능 구현

--- client/src/components/OrderComplete.js
+++ client/src/components/OrderComplete.js
@@ -6,28 +6,36 @@ function OrderComplete({ history }) {
   const navigate = useNavigate();
   return (
     <>
-      <VStack w='100%' h='100vh' align='center' justifyContent='center'>
-        <Box p='2px' w='650px' h='40%' align='center'>
-          <Text fontSize='2xl'>🌷</Text>
-          <Text fontSize='4xl' fontWeight='600'>
+      <VStack w="100%" h="100vh" align="center" justifyContent="center">
+        <Box p="2px" w="650px" h="40%" align="center">
+          <Text fontSize="2xl">🌷</Text>
+          <Text fontSize="4xl" fontWeight="600">
             Thank you!
           </Text>
-          <VStack borderRadius='10px' bg='beige' w='70%' h='40%' m='7px' justify='center'>
-            <Box align='left'>
-              <Text m='3px'>이메일로 주문내역이 발송되었습니다.</Text>
-              <Text m='3px' fontWeight='600'>
+          <VStack
+            borderRadius="10px"
+            bg="beige"
+            w="70%"
+            h="40%"
+            m="7px"
+            justify="center"
+          >
+            <Box align="left">
+              <Text m="3px">이메일로 주문내역이 발송되었습니다.</Text>
+              <Text m="3px" fontWeight="600">
                 주문번호: #ELICE1234567
               </Text>
-              <Text m='3px'>빠르게 제작하여 보내드릴게요!</Text>
+              <Text m="3px">빠르게 제작하여 보내드릴게요!</Text>
             </Box>
           </VStack>
           <Button
             onClick={() => {
               navigate('/');
             }}
-            bgGradient='linear(to-r, orange.200, yellow.300, green.200, teal.200 )'
-            color='white'
-            w='150px'>
+            bgGradient="linear(to-r, orange.200, yellow.300, green.200, teal.200 )"
+            color="white"
+            w="150px"
+          >
             홈으로!
           </Button>
         </Box>
