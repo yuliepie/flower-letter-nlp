39a3a4be592c89aec088179709581fba529cb52a
yuliepie <yuliekorea@gmail.com>
2021-12-09 16:04:58 +0900

    ADD: huggingface transformer docker

--- ml/model-api/app/main.py
+++ ml/model-api/app/main.py
@@ -5,6 +5,9 @@ from pydantic import BaseModel
 from app.classify_emotion import predict as predict_emotion
 from app.classify_keywords import predict as predict_keywords
 
+# test
+from app.keyword_predict import predict as keyword_predict_test
+
 
 class Letter(BaseModel):
     text: str
@@ -22,10 +25,12 @@ def read_root():
     return {"message": "모델 API 입니다!"}
 
 
-@app.get("/classify", response_model=Keywords)
+@app.post("/classify", response_model=Keywords)
 def classify_letter(data: Letter):
     # 모델로 키워드 추출 후 반환
 
     print(data.text)
 
-    return ["우정, 사랑, 여행", "낭만", "도시", "일탈"]
+    keyword_predict_test(data.text)
+
+    return ["우정, 사랑, 여행", "낭만@", "도시", "일탈"]
