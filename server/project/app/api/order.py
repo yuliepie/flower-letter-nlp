90e0c92b402df12a6629cc13e87c5650d3c09317
[2기-1반] 정율리 <yuliekorea@gmail.com>
2021-12-10 03:00:26 +0000

    [BE] 카드결제 & 문의 API 추가

--- server/project/app/api/order.py
+++ server/project/app/api/order.py
@@ -5,11 +5,12 @@ from beanie.odm.fields import PydanticObjectId
 from fastapi import (
     APIRouter,
     Depends,
-    status,
+    Body,
     HTTPException,
     BackgroundTasks,
     requests,
     Request,
+    Response,
 )
 from requests.auth import HTTPBasicAuth
 from fastapi_mail import FastMail, MessageSchema
@@ -21,47 +22,77 @@ from app.models.book import (
     PoemPageModel,
     BookModel,
 )
-from app.models.order import OrderIn, OrderOut, create_order
+from app.models.schemas import ResponseMessage
+from app.models.order import OrderIn, OrderOut, create_order, update_order_status
 from app.db import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-order_router = APIRouter(tags=["Order"])
-
-clientId = "S2_93226cc455504b0a910b7ab5a804683b"
-secretKey = "4696da89c2d64cea80ad520ac1974195"
+order_router = APIRouter(tags=["주문"])
 
 
-@order_router.post("/pay")
-async def pay(request: Any):
+@order_router.post("/pay", summary="결제")
+async def pay(
+    request: Request,
+    background_tasks: BackgroundTasks,
+    response: Response,
+    config: Settings = Depends(get_config),
+):
+    """
+    결제요청.
+
+    - FE에서 주문번호로 결제요청을 보내면, PG사를 거쳐 Server side auth를 위해 이 주소로 POST요청이 오게 된다.
+    - 인증을 거친 후 pg사의 결제승인 API로 요청을 보내 결제 성공/실패 메시지를 받는다.
+    - 결제 성공시 주문의 상태를 '결제완료'로 변경 후, 사용자에게 이메일 발송.
+    - 결제 성공/실패 여부를 반환.
+    """
     try:
+        request_body = await request.json()
+
+        if request_body.form["authResultCode"] != "0000":
+            raise requests.exceptions.RequestException
+
         response = requests.post(
             "https://sandbox-api.nicepay.co.kr/v1/payments/" + request.form["tid"],
             json={"amount": request.form["amount"]},
             headers={"Content-type": "application/json"},
-            auth=HTTPBasicAuth(clientId, secretKey),
+            auth=HTTPBasicAuth(config.client_id, config.secret_key),
         )
 
+        # print(resDict)
         resDict = json.loads(response.text)
-        print(resDict)
+
+        if resDict["resultCode"] != "0000":
+            raise requests.exceptions.RequestException
 
         # 결제 비즈니스 로직 구현
+        orderId = resDict["orderId"]
+        order = await update_order_status(order_id=orderId, status=2)  # 결제 완료 상태
+        background_tasks.add_task(send_email, order.email, order)  # 이메일 전송
 
-        return render_template("response.html", resultMsg=resDict["resultMsg"])
+        return Response(status_code=200)
 
     except requests.exceptions.RequestException as e:
-        raise SystemExit(e)
+        return Response(status_code=400)
 
 
-@order_router.post("/orders", response_model=OrderOut, status_code=201)
+@order_router.post(
+    "/orders", response_model=OrderOut, status_code=201, summary="주문 & 시집 생성"
+)
 async def post_order(
-    background_tasks: BackgroundTasks,
     request: OrderIn,
     db: AsyncSession = Depends(get_db),
 ):
+    """
+    주문 & 시집 데이터 생성.
+    - FE에서 주문정보 & 시집정보를 받는다.
+    - 사용자가 결제하기를 눌렀을때 호출되며, 주문정보를 반환한다.
+    - 주문정보 데이터를 FE에서 성공적으로 받은후에 /pay(POST)를 호출해 결제를 진행하게 된다.
+    """
     new_contents = []
     for req_content in request.book.contents:
         # TODO: use enum class
+        # TODO: add backend logic to verify price
         if req_content.type == "poem":
             poem = PoemPageModel(poem_id=PydanticObjectId(req_content.poem_id))
             new_contents.append(poem)
@@ -76,9 +107,6 @@ async def post_order(
 
     await new_book.create()
     new_order = await create_order(request.order, str(new_book.id), db)
-
-    # 주문확인 이메일 전송
-    background_tasks.add_task(send_email, "yuliekorea@gmail.com", new_order)
 
     return new_order
 
