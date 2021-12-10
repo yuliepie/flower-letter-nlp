05c808e61bde0a6b609724e71faa4eaecab9f5ce
555cider <sosnsg@gmail.com>
2021-12-10 23:37:02 +0900

    ADD: emotion predictor

--- ml/model-api/app/main.py
+++ ml/model-api/app/main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-from app.classify_emotion import predict as predict_emotion
+# AI 관련
+import torch
+from kobert.pytorch_kobert import get_pytorch_kobert_model
 from app.classify_keywords import predict as predict_keywords
-
-# test
-from app.keyword_predict import predict as keyword_predict_test
+from app.classify_emotion import predict as predict_emotion
+from app.keyword_predict import predict as predict_k, BERTClassifier as BERTClassifier_k
+from app.emotion_predict import predict as predict_e, BERTClassifier as BERTClassifier_e
 
 
 class Letter(BaseModel):
@@ -34,25 +36,20 @@ def read_root():
     return {"message": "모델 API 입니다!"}
 
 
-
-
-
-
-#app 실행할때 처음 생성하는 모델
-import torch
-from kobert.pytorch_kobert import get_pytorch_kobert_model
-from app.keyword_predict import BERTClassifier
-
-bertmodel, vocab = get_pytorch_kobert_model(cachedir=".cache")
-
 device = torch.device("cpu")
-saved_model = BERTClassifier(bertmodel,  dr_rate=0.5).to(device)
+
+# 키워드
+model_k, vocab_k = get_pytorch_kobert_model(cachedir=".cache")
+saved_model_k = BERTClassifier_k(model_k, dr_rate=0.5).to(device)
+
+# 감정
+model_e, vocab_e = get_pytorch_kobert_model(cachedir=".cache")
+saved_model_e = BERTClassifier_e(model_e, dr_rate=0.5).to(device)
+
 
 @app.post("/classify", response_model=Keywords)
 def classify_letter(data: Letter):
-    # 모델로 키워드 추출 후 반환
-
     print(data.text)
-    keyword_predict_test(saved_model, vocab, data.text)
-
-    return {"keywords": ["우정", "사랑", "여행", "낭만@", "도시", "일탈"]}
+    predict_k(saved_model_k, vocab_k, data.text)
+    emotion = predict_e(saved_model_e, vocab_e, data.text)
+    return {"keywords": [emotion]}
