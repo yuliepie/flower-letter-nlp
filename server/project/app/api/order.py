d4817715b5b50e966d895307529cdb095eac213b
yuliepie <yuliekorea@gmail.com>
2021-12-10 20:52:17 +0900

    ADD: email format & test

--- server/project/app/api/order.py
+++ server/project/app/api/order.py
@@ -1,20 +1,14 @@
-from os import name
 from typing import Any
 import json
 from beanie.odm.fields import PydanticObjectId
 from fastapi import (
     APIRouter,
     Depends,
-    Body,
-    status,
-    HTTPException,
     BackgroundTasks,
     requests,
-    Request,
     Response,
 )
 from requests.auth import HTTPBasicAuth
-from fastapi_mail import FastMail, MessageSchema
 
 from app.config import Settings, get_config
 
@@ -23,12 +17,17 @@ from app.models.book import (
     PoemPageModel,
     BookModel,
 )
-from app.models.schemas import ResponseMessage
-from app.models.order import OrderIn, OrderOut, create_order, update_order_status
+from app.models.order import (
+    OrderIn,
+    OrderOut,
+    create_order,
+    update_order_status,
+    get_order,
+)
 from app.db import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-
-config = get_config()
+from app.helpers.email import send_email
+from app.models.book import FlowerModel
 
 order_router = APIRouter(tags=["주문"])
 
@@ -38,6 +37,7 @@ async def pay(
     request: Any,
     background_tasks: BackgroundTasks,
     config: Settings = Depends(get_config),
+    db: AsyncSession = Depends(get_db),
 ):
     """
     결제요청.
@@ -67,9 +67,17 @@ async def pay(
             raise requests.exceptions.RequestException
 
         # 결제 비즈니스 로직 구현
-        orderId = resDict["orderId"]
-        order = await update_order_status(order_id=orderId, status=2)  # 결제 완료 상태
-        background_tasks.add_task(send_email, order.email, order)  # 이메일 전송
+        order_id = resDict["orderId"]
+        order = await get_order(order_id, db)
+        book = await BookModel.get(PydanticObjectId(order.book_id))
+        await update_order_status(order_id, db, 2)  # 결제 완료 상태
+
+        # 꽃 이름
+        flower = await FlowerModel.get(PydanticObjectId(book.flower_id))
+
+        background_tasks.add_task(
+            send_email, config, order, book, flower.name
+        )  # 이메일 전송
 
         return Response(status_code=200)
 
@@ -110,110 +118,3 @@ async def post_order(
     new_order = await create_order(request.order, str(new_book.id), db)
 
     return new_order
-
-
-async def send_email(email: str):
-    # TODO: 예쁜 이메일 템플릿 만들기
-    # 139, 143, 147, 151 > 옵션, 170, 174, 178, 182 > 배송정보
-
-    html = """
-<html style="font-family:Arial, Helvetica, sans-serif; font-size: 0.8rem; width: 800px; height: 1500px">
-  <head>
-    <style>
-        @font-face {
-            font-family: "sungsil";
-            src: url(https://flower-letter-resources.s3.ap-northeast-2.amazonaws.com/sungsil.ttf);
-        }
-    </style>
-  </head>
-  <body>
-    <div style="padding-left: 10vw; padding-right: 10vw;">
-      <div style="background-color: #f2c0c6; display:flex;">
-        <img style="width: 150px; margin-left: auto; margin-right: auto;" src="https://flower-letter-resources.s3.ap-northeast-2.amazonaws.com/logo_withoutbg.png" />
-      </div>
-      <div style="background-color: #faf4f4; padding: 20px; padding-left: 20%; padding-right: 20%;">
-        <div style="font-size: 1.2rem; font-weight: bold; padding: 20px; text-align: center; margin-left:auto; margin-right:auto;">
-          정율리 님, <br/> 꽃편지를 이용해 주셔서 감사합니다.
-        </div>
-        <div>
-          <div style="font-size: 1.2rem; font-weight: bold; margin-left: auto; margin-right: auto; padding: 10px; text-align: center;">Order Summary</div>
-          <div style="margin-left: auto; margin-right: auto; font-size: 0.9rem; text-align: center;">Order No. 4729811</div>
-          <div style="margin-left: auto; margin-right: auto; display:flex; width: 427px;">
-            <div>
-              <img src="https://flower-letter-resources.s3.ap-northeast-2.amazonaws.com/sample_img.png" style="height: 180px;"/>
-            </div>
-            <div style="margin-left: 10px; border-bottom: 2px solid #846c6c; border-top: 2px solid #846c6c; padding:0; height: 176px;">
-              <table style="width: 300px; min-width: 180px; border-bottom: 1px solid #846c6c; padding: 15px; display: flex; flex-direction: column;">
-                <tr style="display: flex; justify-content: space-between; width: 270px;">
-                  <td style="font-weight: bold; text-align: left; width: 135px;">Book Color</td>
-                  <td style="text-align: right; width: 135px;">White</td>
-                </tr>
-                <tr style="display: flex; justify-content: space-between; width: 270px;">
-                  <td style="font-weight: bold; text-align: left; width: 135px;">Flower</td>
-                  <td style="text-align: right; width: 135px;">Rose</td>
-                </tr>
-                <tr style="display: flex; justify-content: space-between; width: 270px;">
-                  <td style="font-weight: bold; text-align: left; width: 135px;">Font</td>
-                  <td style="text-align: right; width: 135px;">성실체</td>
-                </tr>
-                <tr style="display: flex; justify-content: space-between; width: 270px;">
-                  <td style="font-weight: bold; text-align: left; width: 135px;">Title</td>
-                  <td style="text-align: right; width: 135px;">너에게 쓰는 편지</td>
-                </tr>
-              </table>
-              <table style="width: 300px; min-width: 180px; padding: 15px;">
-                <tr style="display: flex; justify-content: space-between; width: 270px">
-                  <td style="font-weight: bold; text-align: left; width: 135px;">Price</td>
-                  <td style="text-align: right; width: 135px;">₩ 45,000</td>
-                </tr>
-              </table>
-            </div>
-          </div>
-        </div>
-        <div style="margin-top: 20px; margin-bottom: 30px;">
-          <div style="font-size: 1.2rem; font-weight: bold; padding: 20px; margin-left: auto; margin-right: auto; text-align: center;">
-            Delivery Info
-          </div>
-          <table style="border-left: 2px solid #846c6c; padding-left: 20px; margin-left: auto; margin-right: auto; width: 430px;">
-            <tr style="width: 360px; padding: 3px;">
-              <td style="font-size: 0.9rem; font-weight: bold; width: 80px;"> 받는분 </td>
-              <td>김상훈</td>
-            </tr>
-            <tr style="width: 360px; padding: 3px;">
-              <td style="font-size: 0.9rem; font-weight: bold; width: 80px;">연락처</td>
-              <td>010-1234-5678</td>
-            </tr>
-            <tr style="width: 360px; padding: 3px;">
-              <td style="font-size: 0.9rem; font-weight: bold; width: 80px;">우편번호</td>
-              <td>32373</td>
-            </tr>
-            <tr style="width: 360px; padding: 3px;">
-              <td style="font-size: 0.9rem; font-weight: bold; width: 80px;">배송지</td>
-              <td>서울시 서대문구 연희로 00길 99 xx빌라 123호</td>
-            </tr>
-          </table>
-        </div>
-      </div>
-      <div style="display: flex; background-color: #f2c0c6; padding: 20px;">
-        <div style="font-size: 0.7rem; text-align: center; color: #444; margin-left:auto; margin-right:auto; width:100%;">
-          <div>010-1212-3434</div>
-          <div style="text-decoration: none !important;">yulie@flowerletter.co.kr</div>
-          <div>서울시 강남구 언주로 000, xx빌딩</div>
-          <div>© 2021 꽃편지</div>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
-    """
-
-    message = MessageSchema(
-        subject="주문확인서 - 정율리님",
-        recipients=[email],  # List of recipients
-        html=html,
-        subtype="html",
-    )
-
-    fm = FastMail(config.email_config)
-    await fm.send_message(message)
-    print("email sent successfully.")
