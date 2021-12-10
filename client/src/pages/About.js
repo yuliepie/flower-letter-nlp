359d1dc242efaf259141aef1760090975588faa4
JSKIM <myjunee3608@gmail.com>
2021-12-10 15:43:32 +0900

    STYLE: backgroudcolor-aboutus, howtouse

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -8,34 +8,43 @@ const About = () => {
   return (
     <div style={{ height: '100vh', overflow: 'auto' }}>
       <SidebarComponent></SidebarComponent>
-      <WithBackgroundImage text={'꽃 편지 About us'} textcolor={'black'} />
+      {/* 첫번째 스크롤 */}
+      <WithBackgroundImage
+        text={'꽃 편지 About us'}
+        textcolor={'black'}
+        bgGradient={'linear(to-b, #D4BBDD, #FFE6A8)'}
+      />
+      {/* 두번째 스크롤 */}
       <WithBackgroundImage
         h={'700px'}
         text={
           '대통령의 선거에 관한 사항은 법률로 정한다. 국회의원은 그 지위를 남용하여 국가·공공단체 또는 기업체...'
         }
+        bgGradient={'linear(to-b, #FFE6A8, #D4BBDD)'}
       />
+      {/* 세번째 스크롤 */}
       <LeftImage
         w="full"
         h="700px"
-        imgUrl={'/img/mainPageFirstImg.png'}
+        bgGradient={'linear(to-t, #FFE6A8, #D4BBDD)'}
         text={
           '대통령의 선거에 관한 사항은 법률로 정한다. 국회의원은 그 지위를 남용하여 국가·공공단체 또는 기업체...'
         }
         backgroundImg={'/img/backgroundImg2_fall.png'}
       />
-
+      {/* 네번째 스크롤 */}
       <RightImage
         w="full"
         h="700px"
-        imgUrl={'/img/mainPageFirstImg.png'}
         text={'늘 당신과 함께하고 싶습니다'}
-        backgroundcolor={'#FBEDE0'}
+        backgroundcolor={'#FFE6A8'}
       />
+      {/* 다섯번째 스크롤 */}
       <WithBackgroundImage
         text={
-          '대통령의 선거에 관한 사항은 법률로 정한다. 국회의원은 그 지위를 남용하여 국가·공공단체 또는 기업체...'
+          '대통령의 선거에 관한 사항은 법률로 정한다. 국회의원은 그 지위를 남용하여 국가·공공단체 또는 기업체...💐🌸🏵🌹🌺🌻🌼🌷🥀🍀🍁'
         }
+        bgGradient={'linear(to-t, #D4BBDD, #FFE6A8)'}
       />
     </div>
   );
--- client/src/pages/HowToUse.js
+++ client/src/pages/HowToUse.js
@@ -8,32 +8,33 @@ export default function HowToUse() {
   return (
     <div style={{ height: '100vh', overflow: 'auto' }}>
       <SidebarComponent />
-      <WithBackgroundImage
-        text={'How To Use'}
-        textcolor={'black'}
-        backgroundImg={'/img/lavenderBook.png'}
-      />
+      {/* 첫번째 스크롤 - 이미지로 넣기 */}
+      <WithBackgroundImage text={'How To Use'} textcolor={'black'} />
+      {/* 두번째 스크롤 */}
       <LeftImage
         w="full"
         h="700px"
         imgUrl={'/img/mainpageThirdImg.png'}
         text={'Step1. 편지를 작성해보세요'}
+        backgroundcolor={'#F7EBE9'}
       />
+      {/* 세번째 스크롤 */}
       <RightImage
         w="full"
         h="700px"
         imgUrl={'/img/mainPageFirstImg.png'}
         text={'Step2. 꽃말을 선택하세요'}
-        backgroundcolor={'#FBEDE0'}
+        bgGradient={'linear(to-b, #F7EBE9, #C9B5B3)'}
       />
-
+      {/* 네번째 스크롤 */}
       <LeftImage
         w="full"
         h="700px"
         imgUrl={'/img/mainpageThirdImg.png'}
         text={'Step03. 선택된 시집을 통해 나만의 시집을 만들어보세요'}
+        bgGradient={'linear(to-b, #C9B5B3, #A49393)'}
       />
-
+      {/* 다섯번째 스크롤 */}
       <WithBackgroundImage
         backgroundcolor={'#A49393'}
         text={'지금 바로 시작해보세요'}
