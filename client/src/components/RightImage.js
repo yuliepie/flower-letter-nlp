fac6906a6a7301e0e395575254ab9b39bcea69ac
JSKIM <myjunee3608@gmail.com>
2021-12-12 00:30:00 +0900

    STYLE: font animation

--- client/src/components/RightImage.js
+++ client/src/components/RightImage.js
@@ -23,6 +23,24 @@ export default function RightImage({
   textColor,
   contentText,
 }) {
+  const saTriggerMargin = 300;
+  const saElementList = document.querySelectorAll('.sa');
+
+  const saFunc = function () {
+    for (const element of saElementList) {
+      if (!element.classList.contains('show')) {
+        if (
+          window.innerHeight >
+          element.getBoundingClientRect().top + saTriggerMargin
+        ) {
+          element.classList.add('show');
+        }
+      }
+    }
+  };
+
+  window.addEventListener('load', saFunc);
+  window.addEventListener('scroll', saFunc);
   return (
     <HStack
       w={w}
@@ -37,9 +55,8 @@ export default function RightImage({
       bgGradient={bgGradient}
     >
       <Center w="60vh" h="full">
-        <VStack>
+        <VStack className="sa sa-right">
           <Text
-            // className="sa sa-right" 스크롤애니메이션 클래스
             lineHeight={1.2}
             color={textColor}
             fontWeight={600}
