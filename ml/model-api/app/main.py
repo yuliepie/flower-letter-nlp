f04793c366dca74af658947a45828ac8b7f67ac8
555cider <sosnsg@gmail.com>
2021-12-11 10:25:53 +0900

    REFACTOR: emotion_predict.py

--- ml/model-api/app/main.py
+++ ml/model-api/app/main.py
@@ -1,15 +1,11 @@
 from typing import List
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-
-# AI 관련
 import torch
 from kobert.pytorch_kobert import get_pytorch_kobert_model
-from app.classify_keywords import predict as predict_keywords
-from app.classify_emotion import predict as predict_emotion
-from app.keyword_predict import predict as predict_k, BERTClassifier as BERTClassifier_k
+
 from app.emotion_predict import predict as predict_e, BERTClassifier as BERTClassifier_e
+from app.keyword_predict import predict as predict_k, BERTClassifier as BERTClassifier_k
 
 
 class Letter(BaseModel):
@@ -22,14 +18,6 @@ class Keywords(BaseModel):
 
 app = FastAPI(title="API service for model")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 @app.get("/")
 def read_root():
@@ -37,19 +25,14 @@ def read_root():
 
 
 device = torch.device("cpu")
-
-# 키워드
-model_k, vocab_k = get_pytorch_kobert_model(cachedir=".cache")
-saved_model_k = BERTClassifier_k(model_k, dr_rate=0.5).to(device)
-
-# 감정
-model_e, vocab_e = get_pytorch_kobert_model(cachedir=".cache")
-saved_model_e = BERTClassifier_e(model_e, dr_rate=0.5).to(device)
+model, vocab = get_pytorch_kobert_model(cachedir=".cache")
+model_e = BERTClassifier_e(model, dr_rate=0.5).to(device)
+model_k = BERTClassifier_k(model, dr_rate=0.5).to(device)
 
 
 @app.post("/classify", response_model=Keywords)
 def classify_letter(data: Letter):
     print(data.text)
-    predict_k(saved_model_k, vocab_k, data.text)
-    emotion = predict_e(saved_model_e, vocab_e, data.text)
+    emotion = predict_e(model_e, vocab, data.text)
+    predict_k(model_k, vocab, data.text)
     return {"keywords": [emotion]}
