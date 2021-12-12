d249b2fa134abfdfecde7e110df2af1c54a0941d
Yulie Jung <yuliekorea@gmail.com>
2021-12-12 19:32:49 +0900

    ADD: poem flower list return from DB

--- server/project/app/models/schemas.py
+++ server/project/app/models/schemas.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel
+from typing import List
 
 
 class ResponseMessage(BaseModel):
     message: str
+
+
+class Emotions(BaseModel):
+    high: List[str] = []
+    medium: List[str] = []
+    low: List[str] = []
+
+
+class ModelResults(BaseModel):
+    emotions: Emotions
+    keywords: List[str]
