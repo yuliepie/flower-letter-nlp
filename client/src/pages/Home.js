2514da50e3aaa5dfe5f9f2888e95a7fda39401f6
[2기 2반] 김서정 <kseoj0921@naver.com>
2021-12-12 00:54:48 +0000

    결제정보 전달 데이터 수정

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,20 +5,8 @@ import Fullpage from '../components/Fullpage';
 const axios = require('axios');
 
 const Home = ({ history }) => {
-  async function testPing() {
-    try {
-      const response = await axios.get('https://api.flowerletter.co.kr/ping');
-      alert(response.data['hello']);
-      console.log(response);
-    } catch (error) {
-      alert('error');
-      console.log(error);
-    }
-  }
-
   return (
     <>
-      <button onClick={testPing}>Test Ping</button>
       <SidebarComponent></SidebarComponent>
       <Fullpage></Fullpage>
     </>
