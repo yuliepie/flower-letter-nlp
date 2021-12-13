b1ff73673c01aba868c18f65e462e8fb068f09ed
Yulie Jung <yuliekorea@gmail.com>
2021-12-13 23:19:46 +0900

    ADD: backoffice app

--- office/project/app/main.py
+++ office/project/app/main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, flash
 import requests
+from config import get_config
+
+config = get_config()
 
 app = Flask(__name__)
+
+app.secret_key = "super secret"
 
 
 @app.route("/")
@@ -11,55 +16,40 @@ def hello_world():
 
 @app.route("/inquiry")
 def inquiry_page():
-    mock_data = [
-        {
-            "id": 0,
-            "title": "나는 0번째 질문, 존재하지않지",
-            "name": "김상훈",
-            "content": "넌 나를 볼 수 없다",
-            "email": "sanghunkim.nz@gmail.com",
-            "date": "2021-12-04",
-            "phone": "010-1234-5678",
-            "replied": False,
-        },
-    ]
+    url = f"{config.api_url}/question"
+    try:
+        response = requests.get(url)
+        inquiries = response.json()
+    except:
+        flash("문의 불러오기 실패.")
 
-    return render_template("inquiry.html", inquiries=mock_data)
+    print(inquiries)
+
+    return render_template("inquiry.html", inquiries=inquiries)
 
 
 @app.route("/inquiry/<inquiry_id>")
 def page(inquiry_id):
-    mock_data = [
-        {
-            "id": 0,
-            "title": "나는 0번째 질문, 존재하지않지",
-            "name": "김상훈",
-            "content": "넌 나를 볼 수 없다",
-            "email": "sanghunkim.nz@gmail.com",
-            "date": "2021-12-04",
-            "phone": "010-1234-5678",
-            "replied": False,
-        },
-    ]
+    url = f"{config.api_url}/question/{inquiry_id}"
+    try:
+        response = requests.get(url)
+        inquiry = response.json()
+    except:
+        flash("문의 불러오기 실패.")
 
-    return render_template(
-        "inquiry_reply.html", inquiry_example=mock_data[int(inquiry_id)]
-    )
+    return render_template("inquiry_reply.html", inquiry_example=inquiry)
 
 
 @app.route("/order")
 def order_page():
-    mock_order_data = [
-        {
-            "id": 1,
-            "name": "김상훈",
-            "email": "sanghunkim.nz@gmail.com",
-            "phone": "010-1234-5678",
-            "date": "2021-12-05",
-            "status": "배송완료",
-        }
-    ]
-    return render_template("order.html", orders=mock_order_data)
+    url = f"{config.api_url}/orders"
+    try:
+        response = requests.get(url)
+        orders = response.json()
+    except:
+        flash("주문목록 불러오기 실패.")
+
+    return render_template("order.html", orders=orders)
 
 
 if __name__ == "__main__":
